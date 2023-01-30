8ecbd84aa175069546d55c2b9d3c505dd0210168
shreyxnsh <shreyanshatwork@gmail.com>
2023-01-31 00:20:04 +0530

    task list implemented

--- app/src/main/java/com/shreyxnsh/tiimi/ui/DashboardFragment.java
+++ app/src/main/java/com/shreyxnsh/tiimi/ui/DashboardFragment.java
@@ -2,23 +2,49 @@ package com.shreyxnsh.tiimi.ui;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.shreyxnsh.tiimi.R;
+import com.shreyxnsh.tiimi.ui.dashboard.TodayFragment;
 
 
 public class DashboardFragment extends Fragment {
 
+    private TextView todayTV, completedTV;
+    private int selectedTabNumber = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        getActivity().getSupportFragmentManager().beginTransaction()
+//                .setReorderingAllowed(true)
+//                .replace(R.id.fragmentContainer, TodayFragment.class, null)
+//                .commit();
+//
+//        todayTV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//
+//        completedTV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+
+    }
+
+    private void selectedTab(int tabNumber) {
 
     }
 
@@ -26,13 +52,11 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        todayTV = view.findViewById(R.id.todayTV);
+        completedTV = view.findViewById(R.id.completedTV);
+        return view;
+
     }
-
-
-//    @Override
-//    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-//        inflater.inflate(R.menu.toolbar_menu,menu);
-//        super.onCreateOptionsMenu(menu, inflater);
-//    }
 }
